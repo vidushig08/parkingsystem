3d4c6789f4116571f4edda7df1d3e805db831d71
vidushig08 <vidushi.gupta2019@vitstudent.ac.in>
2021-07-09 20:13:52 +0530

    images added

--- src/components/main.js
+++ src/components/main.js
@@ -2,9 +2,14 @@ import React from 'react'
 //import { Link } from 'react-router-dom';
 //import { useHistory } from 'react-router-dom';
 import './home.css'
+import Parking from './parking2.jpg';
 function Main() {
     return (
-        <div class="firstdiv">
+        <div>
+            <div className="leftdiv">
+                <img src={Parking} alt="" />
+            </div>
+            <div class="firstdiv">
             <br />
             <h1>PARKING LOT SYSTEM</h1><br />
             <label>Existing User: </label> &nbsp;&nbsp;&nbsp;&nbsp;
@@ -14,6 +19,8 @@ function Main() {
             <button class="home-btn" id="signup-btn" onClick={event =>  window.location.href='/signup'}>Sign Up</button>
             <br /><br />
         </div>
+        </div>
+        
     )
 }
 export default Main
